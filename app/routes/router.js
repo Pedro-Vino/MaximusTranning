0b3b5476d705682909656a0a9d0d87d5bf1edd94
LucasProenk <lucas2008aas@gmail.com>
2026-03-29 01:30:59 -0300

    add-router

--- app/routes/router.js
+++ app/routes/router.js
@@ -5,4 +5,16 @@ router.get('/', function(req,res){
     res.render('pages/home');  
 })
 
+router.get('/login', function(req,res){
+    res.render('pages/login');  
+})
+
+router.get('/planos', function(req,res){
+    res.render('pages/planos');  
+})
+
+router.get('/compras', function(req,res){
+    res.render('pages/buy');
+})
+
 module.exports = router;
