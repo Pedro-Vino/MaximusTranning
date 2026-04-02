004623ab987a837056edebfbc07b726fc18314dd
Pedro-Vino <pedro.vinisillva@gmail.com>
2026-04-02 15:55:27 -0300

    Correção das rotasAdm parte 2

--- app/routes/routerAdm.js
+++ app/routes/routerAdm.js
@@ -1,56 +1,23 @@
-var express = require('express');
-var router = express.Router();
-const admController = require("../controllers/admController");
-const db = require('../../config/pool-conexoes');
 
+const express = require('express');
+const router = express.Router();
+const admController = require("../controllers/admController");
 const USER = {
     id: "admMaximus",
     senha: "adm123456"
 };
 
-// ✅ HOME (usar só uma rota)
-router.get('/home', async function(req, res){
-    try {
-        const [alunos] = await db.query('SELECT * FROM aluno');
+// HOME
+router.get('/home', admController.home);
 
-        res.render('pages/adm/home', { alunos });
+//LOGIN
+router.get('/login', admController.loginPage);
+//POST
+router.post('/login', admController.login);
 
-    } catch (err) {
-        console.error(err);
-        res.render('pages/adm/home', { alunos: [] });
-    }
-});
-
-// ✅ LOGIN PAGE
-router.get('/login', function(req, res){
-    res.render('pages/adm/login', { erro: null });  
-});
-
-// ✅ LOGIN POST
-router.post('/login', async (req, res) => {
-    const { id, senha } = req.body;
-
-    if (id === USER.id && senha === USER.senha) {
-        try {
-            const [alunos] = await db.query('SELECT * FROM aluno');
-
-            res.render('pages/adm/home', { 
-                usuario: id,
-                alunos
-            });
-
-        } catch (err) {
-            console.error(err);
-
-            res.render('pages/adm/home', { 
-                usuario: id,
-                alunos: []
-            });
-        }
-
-    } else {
-        res.render('pages/adm/login', { erro: "ID ou senha incorretos" });
-    }
-});
+// Página FORMULÁRIO DE ALUNO
+router.get('/aluno', admController.formAluno);
+// POST
+router.post('/aluno', admController.cadastrarAluno);
 
 module.exports = router;
