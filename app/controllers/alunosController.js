c2cabce21080986da8fb160062173bb2f2b8d3c0
Pedro-Vino <pedro.vinisillva@gmail.com>
2026-04-03 18:36:25 -0300

    Desenvolvendo as pag adm (EM MANUTENÇÃO)

---
+++ app/controllers/alunosController.js
@@ -0,0 +1,12 @@
+const UsuarioModel = require('../models/model-usuario');
+const bcrypt = require('bcryptjs');
+const { body, validationResult } = require("express-validator");
+const https = require("https");
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  
+    
+
+  
+};
--- app/routes/routerAdm.js
+++ app/routes/routerAdm.js
@@ -2,22 +2,34 @@
 const express = require('express');
 const router = express.Router();
 const admController = require("../controllers/admController");
+// const alunosController = require("../controllers/alunosController");
+
+const verificarAdm = admController.verificarAdm;
+
 const USER = {
     id: "admMaximus",
     senha: "adm123456"
 };
 
-// HOME
-router.get('/home', admController.home);
+router.get("/login", (req, res) => {
+  res.render("pages/adm/login", {
+  erro: null,  erros: null,  dados: { email: "", senha: "" },  retorno: null
+});
+});
 
-//LOGIN
-router.get('/login', admController.loginPage);
-//POST
-router.post('/login', admController.login);
+router.get('/home',verificarAdm, function(req,res){
+    res.render('pages/adm/home');  
+})
 
-// Página FORMULÁRIO DE ALUNO
-router.get('/aluno', admController.formAluno);
-// POST
-router.post('/aluno', admController.cadastrarAluno);
+
+// //LOGIN
+// router.get('/login', admController.loginPage);
+// //POST
+// router.post('/login', admController.login);
+
+// // Página FORMULÁRIO DE ALUNO
+// router.get('/aluno', admController.formAluno);
+// // POST
+// router.post('/aluno', admController.cadastrarAluno);
 
 module.exports = router;
