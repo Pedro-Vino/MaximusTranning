794802dfbf7b03b0569a3f16b3c89ae2052bcee1
Pedro-Vino <pedro.vinisillva@gmail.com>
2026-04-07 14:46:09 -0300

    router "novo"

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,9 +1,14 @@
+// 
+
+
+// router novo 
+
 var express = require('express');
 var router = express.Router();
+
 const guestMiddleware = require('../helpers/guestMiddleware');
 const UsuariotesteController = require('../controllers/usuariotesteController');
 const alunosController = require('../controllers/alunosController');
-
 
 // AUTENTICACAO
 function verificarAutenticacao(req, res, next) {
@@ -13,107 +18,133 @@ function verificarAutenticacao(req, res, next) {
   res.redirect("/login");
 }
 
-
-
-// Rotas de páginas estáticas
-router.get('/', function(req, res){
-    res.render('pages/home');  
-});
-
-router.get('/planos', function(req, res){
-    res.render('pages/planos');  
-});
-
-router.get('/compras', function(req, res){
-    res.render('pages/buy');
-});
-
-
-
-
-
+// Disponibiliza aluno na view
 router.use((req, res, next) => {
   res.locals.aluno = req.session.aluno || null;
   next();
 });
 
-router.get('/cadastro', guestMiddleware, (req, res) => {
-  res.render('pages/registro', 
-  { "erros": null, "dados": {"email":"","senha":""},dadosNotificacao:"","retorno":null });
+// =======================
+// ROTAS PÁGINAS
+// =======================
+
+router.get('/', (req, res) => {
+  res.render('pages/home');  
 });
 
-router.post('/cadastrar',
-  alunosController.regrasValidacao, 
-  alunosController.cadastrarAlunoNormal);
+router.get('/planos', (req, res) => {
+  res.render('pages/planos');  
+});
 
-router.get(
-  "/ativar-conta",
-  async function (req, res) {
-    alunosController.ativarConta(req, res);
-  }
+router.get('/compras', (req, res) => {
+  res.render('pages/buy');
+});
+
+// =======================
+// CADASTRO
+// =======================
+
+router.get('/cadastro', guestMiddleware, (req, res) => {
+  res.render('pages/registro', { 
+    erros: null, 
+    dados: { email: "", senha: "" },
+    dadosNotificacao: "",
+    retorno: null 
+  });
+});
+
+router.post(
+  '/cadastrar',
+  alunosController.regrasValidacao,
+  alunosController.cadastrarAlunoNormal
 );
+
+// =======================
+// ATIVAÇÃO DE CONTA
+// =======================
+
+router.get('/ativar-conta', alunosController.ativarConta);
+
+// =======================
+// LOGIN / LOGOUT
+// =======================
 
 router.get("/login", (req, res) => {
   res.render("pages/login", {
-  erro: null,  erros: null,  dados: { email: "", senha: "" }, dadosNotificacao:"",  retorno: null
-});
+    erro: null,
+    erros: null,
+    dados: { email: "", senha: "" },
+    dadosNotificacao: "",
+    retorno: null
+  });
 });
 
-router.post("/login",   alunosController.regrasValidacaoLogin, alunosController.autenticar);
+// ✅ CORREÇÃO AQUI
+router.post(
+  "/login",
+  alunosController.regrasValidacaoLogin,
+  alunosController.autenticarAluno
+);
 
-// Rota de logout
 router.get("/logout", alunosController.logout);
-router.post("/logout", alunosController.logout);  
+router.post("/logout", alunosController.logout);
 
-router.get("/recuperar-senha", 
-  function(req, res){
-    res.render("pages/recuperar-senha",
-      { erros: null, dadosNotificacao: null });
-});
+// =======================
+// RECUPERAR SENHA
+// =======================
 
-router.post("/recuperar-senha",
-  alunosController.regrasValidacaoFormRecSenha, 
-  function(req, res){
-    alunosController.recuperarSenha(req, res);
-});
-
-router.get("/reset-senha", 
-  function(req, res){
-    alunosController.validarTokenNovaSenha(req, res);
+router.get("/recuperar-senha", (req, res) => {
+  res.render("pages/recuperar-senha", {
+    erros: null,
+    dadosNotificacao: null
   });
-
-  
-router.get("/reset-senha-teste", 
-  function(req, res){
-      res.render("pages/resetar-senha",
-      { erros: null, dadosNotificacao: null,"usu_id":"" });
-  });
-  
-router.post("/resetar-senha", 
-    alunosController.regrasValidacaoFormNovaSenha,
-  function(req, res){
-    alunosController.resetarSenha(req, res);
 });
 
+router.post(
+  "/recuperar-senha",
+  alunosController.regrasValidacaoFormRecSenha,
+  alunosController.recuperarSenha
+);
 
+// =======================
+// RESET SENHA
+// =======================
 
+router.get("/reset-senha", alunosController.validarTokenNovaSenha);
 
+router.get("/reset-senha-teste", (req, res) => {
+  res.render("pages/resetar-senha", {
+    erros: null,
+    dadosNotificacao: null,
+    usu_id: ""
+  });
+});
 
-// Rotas de cadastro
-router.get('/cadastro/teste', function(req, res){
-    res.render('pages/testesdoPedro');  
+router.post(
+  "/resetar-senha",
+  alunosController.regrasValidacaoFormNovaSenha,
+  alunosController.resetarSenha
+);
+
+// =======================
+// ROTAS TESTE
+// =======================
+
+router.get('/cadastro/teste', (req, res) => {
+  res.render('pages/testesdoPedro');  
 });
 
 router.get('/cadastro/form', UsuariotesteController.formCadastro);
-
 router.post('/cadastro', UsuariotesteController.cadastrar);
 
-// Rotas de usuários
+// =======================
+// USUÁRIOS
+// =======================
+
 router.get('/usuarios', UsuariotesteController.listar);
 
-router.get('/usuarios/view', function(req, res){
-    res.render('pages/usuarios');
+router.get('/usuarios/view', (req, res) => {
+  res.render('pages/usuarios');
 });
-
 
 module.exports = router;
